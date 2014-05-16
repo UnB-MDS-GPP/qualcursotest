d24ec2838cbc8a30a060e777f619a17f17f497d8
André Bernardes <andrebsguedes@gmail.com>
2014-05-15 23:25:45 -0300

    MainActivity tests

--- src/unb/mdsgpp/qualcurso/test/TestMainActivity.java
+++ src/unb/mdsgpp/qualcurso/test/TestMainActivity.java
@@ -1,6 +1,7 @@
 package unb.mdsgpp.qualcurso.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import unb.mdsgpp.qualcurso.InstitutionListFragment;
 import unb.mdsgpp.qualcurso.MainActivity;
 import unb.mdsgpp.qualcurso.R;
 
@@ -21,18 +22,12 @@ public class TestMainActivity extends ActivityInstrumentationTestCase2<MainActiv
 
 	public void testShouldOnSectionAttachedSetTheActivityTitle() {
 		this.mActivity.onSectionAttached(1);
-		assertEquals(this.mActivity.getActionBar().getTitle(), this.mActivity.getString(R.string.title_section1));
-
-		this.mActivity.onSectionAttached(2);
-		assertEquals(this.mActivity.getActionBar().getTitle(), this.mActivity.getString(R.string.title_section2));
-
-		this.mActivity.onSectionAttached(3);
-		assertEquals(this.mActivity.getActionBar().getTitle(), this.mActivity.getString(R.string.title_section3));
+		assertEquals(this.mActivity.getString(R.string.app_name), this.mActivity.getActionBar().getTitle());
+		
 	}
 
-	/*public void testShouldonNavigationDrawerItemSelectedSetInstitutionListFragment() {
-		this.mActivity.onNavigationDrawerItemSelected(0);
-
-		this.mActivity.getFragmentManager().
-	}*/
+	public void testShouldonNavigationDrawerItemSelectedSetInstitutionListFragment() {
+		//this.mActivity.onNavigationDrawerItemSelected(0);
+		assertEquals(InstitutionListFragment.class, this.mActivity.getSupportFragmentManager().findFragmentById(R.id.container).getClass());
+	}
 }
