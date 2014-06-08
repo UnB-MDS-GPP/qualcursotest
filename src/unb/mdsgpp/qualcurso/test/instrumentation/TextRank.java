2ca83b05b9adc16aac14b905235d08aabd2a2e21
Fabio Teixeira <fabio1079@gmail.com>
2014-06-08 19:30:29 -0300

    Removing controllers tests from MainActivty test file and adding to a new package "Instrumentation"
    (testing_ranking)

---
+++ src/unb/mdsgpp/qualcurso/test/instrumentation/TextRank.java
@@ -0,0 +1,49 @@
+package unb.mdsgpp.qualcurso.test.instrumentation;
+
+import unb.mdsgpp.qualcurso.MainActivity;
+import unb.mdsgpp.qualcurso.R;
+import android.app.Instrumentation;
+import android.support.v4.app.Fragment;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+
+public class TextRank  extends ActivityInstrumentationTestCase2<MainActivity>  {
+	private MainActivity mActivity;
+	private Instrumentation mInstrumentation;
+
+	public TextRank() {
+		super(MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		setActivityInitialTouchMode(false);
+		this.mActivity = getActivity();
+		this.mInstrumentation = getInstrumentation();
+	}
+
+	public void testShouldNotAllowRankWithoutIndicator() {
+		Fragment nd = this.mActivity.getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+		ListView nl = (ListView)nd.getView().findViewById(R.id.navigation_list_view);
+		
+		TouchUtils.clickView(this, nl.getChildAt(2));
+		Fragment rank = this.mActivity.getSupportFragmentManager().findFragmentById(R.id.container);
+		
+		final AutoCompleteTextView course = (AutoCompleteTextView) rank.getView().findViewById(R.id.autoCompleteTextView);
+		mActivity.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				course.setText("eng");
+			}
+		});
+		View v = course.getTouchables().get(0);
+		TouchUtils.clickView(this, v);
+		ListView evaluationList = (ListView) rank.getView().findViewById(R.id.evaluationList);
+		assertNull(evaluationList.getAdapter());
+	}
+}
