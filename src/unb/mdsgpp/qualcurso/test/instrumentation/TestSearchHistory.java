6ededff7bfa8dac251a04b027a279274450ce0f1
Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>
2014-06-17 00:26:08 -0300

    create new test testShouldShowIntensSearch
    (testing_search_history)
    
    Signed-off-by: Brenddon Gontijo <brenddongontijo@msn.com>
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Jonnatas Lennon Lima Costa <jonatas_lenon@hotmail.com>

--- src/unb/mdsgpp/qualcurso/test/instrumentation/TestSearchHistory.java
+++ src/unb/mdsgpp/qualcurso/test/instrumentation/TestSearchHistory.java
@@ -5,10 +5,17 @@ import helpers.Indicator;
 import java.sql.Date;
 import java.util.Calendar;
 
+import models.Course;
 import models.Evaluation;
+import models.Institution;
 import models.Search;
+import unb.mdsgpp.qualcurso.CourseListFragment;
+import unb.mdsgpp.qualcurso.EvaluationDetailFragment;
+import unb.mdsgpp.qualcurso.HistoryFragment;
+import unb.mdsgpp.qualcurso.InstitutionListFragment;
 import unb.mdsgpp.qualcurso.MainActivity;
 import unb.mdsgpp.qualcurso.R;
+import unb.mdsgpp.qualcurso.SearchByIndicatorFragment;
 import android.app.Instrumentation;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -17,77 +24,106 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Spinner;
 
-public class TestSearchHistory extends ActivityInstrumentationTestCase2<MainActivity>{
+public class TestSearchHistory extends
+		ActivityInstrumentationTestCase2<MainActivity> {
 
-		private MainActivity mActivity;
-		private Instrumentation mInstrumentation;
+	private MainActivity mActivity;
+	private Instrumentation mInstrumentation;
 
-		public TestSearchHistory(){
-			super(MainActivity.class);
-		}
+	public TestSearchHistory() {
+		super(MainActivity.class);
+	}
 
-		@Override
-		protected void setUp() throws Exception {
-			super.setUp();
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
 
-			setActivityInitialTouchMode(false);
-			this.mActivity = getActivity();
-			this.mInstrumentation = getInstrumentation();
+		setActivityInitialTouchMode(false);
+		this.mActivity = getActivity();
+		this.mInstrumentation = getInstrumentation();
+	}
+
+	public void setUpBeforeClass() {
+		Calendar c = Calendar.getInstance();
+		Search s = new Search();
+		s.setIndicator(Indicator.getIndicatorByValue("triennial_evaluation"));
+		s.setMaxValue(10);
+		s.setMinValue(5);
+		s.setOption(1);
+		s.setYear(2010);
+		s.setDate(new Date(c.getTime().getTime()));
+		s.save();
+		s = new Search();
+		s.setIndicator(Indicator.getIndicatorByValue("theses"));
+		s.setMaxValue(20);
+		s.setMinValue(10);
+		s.setOption(0);
+		s.setYear(2007);
+		s.setDate(new Date(c.getTime().getTime()));
+		s.save();
+	}
+
+	public void testPreConditions() {
+		setUpBeforeClass();
+	}
+
+	public void openDrawerOptionAt(int position) {
+		Fragment nd = this.mActivity.getSupportFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+		DrawerLayout mDrawerLayout = (DrawerLayout) mActivity
+				.findViewById(R.id.drawer_layout);
+
+		if (!mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+			View v = nd.getView().focusSearch(View.FOCUS_FORWARD);
+			TouchUtils.clickView(this, v);
 		}
-		
-		public void setUpBeforeClass(){
-			Calendar c = Calendar.getInstance();
-			Search s = new Search();
-			s.setIndicator(Indicator.getIndicatorByValue("triennial_evaluation"));
-			s.setMaxValue(10);
-			s.setMinValue(5);
-			s.setOption(1);
-			s.setYear(2010);
-			s.setDate(new Date(c.getTime().getTime()));
-			s.save();
-			s = new Search();
-			s.setIndicator(Indicator.getIndicatorByValue("theses"));
-			s.setMaxValue(20);
-			s.setMinValue(10);
-			s.setOption(0);
-			s.setYear(2007);
-			s.setDate(new Date(c.getTime().getTime()));
-			s.save();
-		}
-		
-		public void testPreConditions(){
-			setUpBeforeClass();
-		}
-		
-		public void openDrawerOptionAt(int position){
-			Fragment nd = this.mActivity.getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-			DrawerLayout mDrawerLayout = (DrawerLayout) mActivity.findViewById(R.id.drawer_layout);
-			
-			if(!mDrawerLayout.isDrawerOpen(GravityCompat.START)){
-				View v = nd.getView().focusSearch(View.FOCUS_FORWARD);
-				TouchUtils.clickView(this, v);
-			}
-			ListView nl = (ListView)nd.getView().findViewById(R.id.navigation_list_view);
-			TouchUtils.clickView(this, nl.getChildAt(position));
-		}
-		
-		public void testShouldShowLastSearchesMade(){
-			openDrawerOptionAt(3);
-			Fragment history = this.mActivity.getSupportFragmentManager().findFragmentById(R.id.container);
-			ListView historyList = (ListView)history.getView().findViewById(R.id.listHistory);
-			int t;
-			Search s = (Search)historyList.getAdapter().getItem(Search.count()-1);
+		ListView nl = (ListView) nd.getView().findViewById(
+				R.id.navigation_list_view);
+		TouchUtils.clickView(this, nl.getChildAt(position));
+	}
+
+	public void testShouldShowLastSearchesMade() {
+		openDrawerOptionAt(3);
+		Fragment history = this.mActivity.getSupportFragmentManager()
+				.findFragmentById(R.id.container);
+		ListView historyList = (ListView) history.getView().findViewById(
+				R.id.listHistory);
+		int t;
+		Search s = (Search) historyList.getAdapter()
+				.getItem(Search.count() - 1);
+		assertEquals(s.getId(), Search.last().getId());
+	}
+
+	public void testShouldTheTenthSearchMade() {
+		openDrawerOptionAt(3);
+		Fragment history = this.mActivity.getSupportFragmentManager()
+				.findFragmentById(R.id.container);
+		ListView historyList = (ListView) history.getView().findViewById(
+				R.id.listHistory);
+		if (historyList.getAdapter().getCount() == 10) {
+			Search s = (Search) historyList.getAdapter().getItem(9);
 			assertEquals(s.getId(), Search.last().getId());
 		}
-		
-		public void testShouldTheTenthSearchMade(){
-			openDrawerOptionAt(3);
-			Fragment history = this.mActivity.getSupportFragmentManager().findFragmentById(R.id.container);
-			ListView historyList = (ListView)history.getView().findViewById(R.id.listHistory);
-			if(historyList.getAdapter().getCount() == 10){
-				Search s = (Search)historyList.getAdapter().getItem(9);
-				assertEquals(s.getId(), Search.last().getId());
-			}
-		}
+	}
+
+	public void testShouldShowIntensSearch() {
+		openDrawerOptionAt(3);
+		Fragment history = this.mActivity.getSupportFragmentManager()
+				.findFragmentById(R.id.container);
+		ListView historyList = (ListView) history.getView().findViewById(
+				R.id.listHistory);
+		TouchUtils.clickView(this, historyList);
+		/*Fragment course_institution = this.mActivity
+				.getSupportFragmentManager().findFragmentById(R.id.container);
+		final Spinner listSelectionSpinner = (Spinner) course_institution
+				.getView().findViewById(R.id.course_institution);
+		//if ( listSelectionSpinner.getItemAtPosition() == 1) {
+			assertTrue(course_institution instanceof InstitutionListFragment);
+	//	} else if ((boolean) listSelectionSpinner.getItemAtPosition(1)) {
+			assertTrue(course_institution instanceof CourseListFragment);
+		//}*/
+
+	}
 }
